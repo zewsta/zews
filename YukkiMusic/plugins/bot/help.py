2a8d3bdc854ad66a4e58601d3d727cafbf130675
zewsta <mainmusto@gmail.com>
2025-04-16 20:57:24 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -45,7 +45,7 @@ async def format_helper_text(lng, helper_key: str, text: str) -> str:
     def _cmd(key):
         return " ".join(f"/{cmd}" for cmd in _[key])
 
-    if helper_key == "🎧 ᴏʏɴᴀᴛᴍᴀ ᴋᴏᴍᴜᴛʟᴀʀɪ":
+    if helper_key == "ᴏʏɴᴀᴛᴍᴀ ᴋᴏᴍᴜᴛʟᴀʀɪ":
         return text.format(
             _cmd("AUTH_COMMAND"), _cmd("UNAUTH_COMMAND"), _cmd("AUTHUSERS_COMMAND")
         )
@@ -63,7 +63,7 @@ async def format_helper_text(lng, helper_key: str, text: str) -> str:
             _cmd("REBOOT_COMMAND"),
             _cmd("LOOP_COMMAND"),
         )
-    elif helper_key == "ʙᴏᴛ ᴋᴏᴍᴜᴛʟᴀʀɪ 🤖":
+    elif helper_key == "ʙᴏᴛ ᴋᴏᴍᴜᴛʟᴀʀɪ":
         return text.format(
             _cmd("ACTIVEVC_COMMAND"),
             _cmd("ACTIVEVIDEO_COMMAND"),
@@ -169,8 +169,8 @@ async def paginate_modules(page_n, chat_id: int, close: bool = False):
             ),
         ),
         EqInlineKeyboardButton(
-            "💣 ᴋᴀᴘᴀᴛ" if close else "🔙 ɢᴇʀɪ",
-            callback_data="💣 ᴋᴀᴘᴀᴛ" if close else "settingsback_helper",
+            "ᴋᴀᴘᴀᴛ" if close else "ɢᴇʀɪ",
+            callback_data="ᴋᴀᴘᴀᴛ" if close else "settingsback_helper",
         ),
         EqInlineKeyboardButton(
             "❯",
@@ -186,8 +186,8 @@ async def paginate_modules(page_n, chat_id: int, close: bool = False):
         pairs.append(
             [
                 EqInlineKeyboardButton(
-                    "💣 ᴋᴀᴘᴀᴛ" if close else "🔙 ɢᴇʀɪ",
-                    callback_data="💣 ᴋᴀᴘᴀᴛ" if close else "settingsback_helper",
+                    "ᴋᴀᴘᴀᴛ" if close else "ɢᴇʀɪ",
+                    callback_data="ᴋᴀᴘᴀᴛ" if close else "settingsback_helper",
                 )
             ]
         )
@@ -269,10 +269,10 @@ async def help_button(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        text="🔙 ɢᴇʀɪ",
+                        text="ɢᴇʀɪ",
                         callback_data=f"help_prev({prev_page_num},{int(close)})",
                     ),
-                    InlineKeyboardButton(text="💣 ᴋᴀᴘᴀᴛ", callback_data="close"),
+                    InlineKeyboardButton(text="ᴋᴀᴘᴀᴛ", callback_data="close"),
                 ],
             ]
         )
