052f53119a3c1c28a31c1c73bc35b5df04785b08
zewsta <mainmusto@gmail.com>
2025-04-17 17:27:45 +0300

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -24,7 +24,7 @@ from YukkiMusic.utils.decorators.language import language
 async def useradd(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Due to privacy issues, You can't manage sudoers when you are on Yukki Database.\n\n Please fill Your MONGO_DB_URI in your vars to use this features**"
+            "**Gizlilik sorunları nedeniyle, Yukki Veritabanındayken sudoers'ı yönetemezsiniz.\n\n Bu özellikleri kullanmak için lütfen MONGO_DB_URI bilgilerinizi vars olarak doldurun**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
@@ -40,7 +40,7 @@ async def useradd(client, message: Message, _):
             SUDOERS.add(user.id)
             await message.reply_text(_["sudo_2"].format(user.mention))
         else:
-            await message.reply_text("Something wrong happened")
+            await message.reply_text("Yanlış bir şey oldu")
         return
     if message.reply_to_message.from_user.id in SUDOERS:
         return await message.reply_text(
@@ -53,7 +53,7 @@ async def useradd(client, message: Message, _):
             _["sudo_2"].format(message.reply_to_message.from_user.mention)
         )
     else:
-        await message.reply_text("Something wrong happened")
+        await message.reply_text("Yanlış bir şey oldu")
     return
 
 
@@ -62,7 +62,7 @@ async def useradd(client, message: Message, _):
 async def userdel(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Due to privacy issues, You can't manage sudoers when you are on Yukki Database.\n\n Please fill Your MONGO_DB_URI in your vars to use this features**"
+            "**Gizlilik sorunları nedeniyle, Yukki Veritabanındayken sudoers'ı yönetemezsiniz.\n\n Bu özellikleri kullanmak için lütfen MONGO_DB_URI bilgilerinizi vars olarak doldurun**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
@@ -78,7 +78,7 @@ async def userdel(client, message: Message, _):
             SUDOERS.remove(user.id)
             await message.reply_text(_["sudo_4"])
             return
-        await message.reply_text(f"Something wrong happened")
+        await message.reply_text(f"Yanlış bir şey oldu")
         return
     user_id = message.reply_to_message.from_user.id
     if user_id not in SUDOERS:
@@ -88,7 +88,7 @@ async def userdel(client, message: Message, _):
         SUDOERS.remove(user_id)
         await message.reply_text(_["sudo_4"])
         return
-    await message.reply_text(f"Something wrong happened")
+    await message.reply_text(f"Yanlış bir şey oldu")
 
 
 @app.on_message(command("SUDOUSERS_COMMAND") & ~BANNED_USERS)
@@ -103,7 +103,7 @@ async def sudoers_list(client, message: Message, _):
             count += 1
         except Exception:
             continue
-        text += f"{count}➤ {user} (`{x}`)\n"
+        text += f"{count} » {user} (`{x}`)\n"
     smex = 0
     for user_id in SUDOERS:
         if user_id not in OWNER_ID:
@@ -114,7 +114,7 @@ async def sudoers_list(client, message: Message, _):
                     smex += 1
                     text += _["sudo_6"]
                 count += 1
-                text += f"{count}➤ {user} (`{user_id}`)\n"
+                text += f"{count} » {user} (`{user_id}`)\n"
             except Exception:
                 continue
     if not text:
