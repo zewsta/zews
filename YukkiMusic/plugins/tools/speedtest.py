446a135edd8aadef905a75f1b7347db51bdb78eb
zewsta <mainmusto@gmail.com>
2025-04-17 18:12:40 +0300

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -18,13 +18,13 @@ async def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = await m.edit("⇆ Running Download Speedtest ...")
+        m = await m.edit("⇆ **İndirme Hızı Test Ediliyor...**")
         test.download()
-        m = await m.edit("⇆ Running Upload SpeedTest...")
+        m = await m.edit("⇆ **Yükleme Hızı Test Ediliyor...**")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = await m.edit("↻ Sharing SpeedTest results")
+        m = await m.edit("↻ **Hız Testi Sonuçları Paylaşılıyor...**")
     except Exception as e:
         return await m.edit(e)
     return result
@@ -32,19 +32,19 @@ async def testspeed(m):
 
 @app.on_message(command("SPEEDTEST_COMMAND") & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("ʀᴜɴɴɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ")
+    m = await message.reply_text("**Hız Testi Başlıyor...**")
     result = await testspeed(m)
-    output = f"""**Speedtest Results**
+    output = f"""**Hız Testi Sonuçları**
     
-<u>**Client:**</u>
-**ISP :** {result['client']['isp']}
-**Country :** {result['client']['country']}
+<u>**Müşteri:**</u>
+**ISS :** {result['client']['isp']}
+**Ülke :** {result['client']['country']}
   
-<u>**Server:**</u>
-**Name :** {result['server']['name']}
-**Country:** {result['server']['country']}, {result['server']['cc']}
-**Sponsor:** {result['server']['sponsor']}
-**Latency:** {result['server']['latency']}  
+<u>**Sunucu:**</u>
+**Ad :** {result['server']['name']}
+**Ülke :** {result['server']['country']}, {result['server']['cc']}
+**Sponsor :** {result['server']['sponsor']}
+**Gecikme :** {result['server']['latency']}  
 **Ping :** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=result["share"], caption=output
